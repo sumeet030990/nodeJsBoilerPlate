7f710fef74594130fddf3fca333a3e555d60b8d3
Sumeet <sumeet.jadhav@e-zest.in>
2022-02-18 23:44:46 +0530

    eslint prettier husky setup completed

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+const port: number = 3000
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

b8ed4ab434d845eeb794e8e81b7e1dd5e04b0570
Sumeet <sumeet.jadhav@e-zest.in>
2022-02-22 19:45:47 +0530

    implemented scalability of server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import express, { Application, Request, Response } from 'express';
+import cluster from 'cluster';
+import config from 'config';
+import { cpus } from 'os';
+import http from 'http';
+
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
 
 const app: Application = express();
-const port: number = 3000;
+const port: number | string = config.get('PORT') || 5000;
+const numCPUs: number = cpus().length;
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+if (cluster.isPrimary) {
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', () => {
+    cluster.fork(); // create new process if any process is died
+  });
+} else {
+  http.createServer().listen(port);
+  // eslint-disable-next-line no-console
+  console.log(` ${config.get('APP_NAME')} app listening on port ${port} with process id ${process.pid}!`);
+}
